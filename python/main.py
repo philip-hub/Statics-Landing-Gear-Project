b31e919ab69056601b8ec3f9fb12b3ac438ab0ae
philip-hub <philipstudentemail@gmail.com>
2023-01-16 10:00:00 -0500

    added shear

--- python/main.py
+++ python/main.py
@@ -1,6 +1,6 @@
 # Y
 # ^  <-
-# |   |M
+# |   |+M
 # ___>X
 
 #angle defined from 0,0 in degrees going counter clockwise to the x axsis
@@ -19,3 +19,10 @@ def stress(force, area):
 def normalStress(force, angle, area):
     nForce = float(math.cos(math.radians(angle)))*float(force)
     return(stress(nForce,area))
+
+def shearStress(force, angle, area):
+    nForce = float(math.sin(math.radians(angle)))*float(force)
+    return(stress(nForce,area))
+
+
+# for i in range (0,90):
