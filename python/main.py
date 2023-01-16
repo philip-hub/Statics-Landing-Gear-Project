e1567a6fd09944981585978afbc44c5c263dbad5
philip-hub <philipstudentemail@gmail.com>
2023-01-16 09:27:19 -0500

    normal force

--- python/main.py
+++ python/main.py
@@ -1,3 +1,16 @@
+import math
+
+
 def stress(force, area):
     return(force/area)
 
+# Y
+# ^  <-
+# |   |M
+# ___>X
+
+#angle defined from 0,0 in degrees going counter clockwise to the x axsis
+
+def normalStress(force, angle, area):
+    nForce = float(math.cos(math.radians(angle)))*float(force)
+    return(stress(nForce,area))
