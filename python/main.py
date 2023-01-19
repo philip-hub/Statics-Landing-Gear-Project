1cff8d0244c6ab1aae43bf541d0c99d19ce58d8f
philip-hub <philipstudentemail@gmail.com>
2023-01-19 17:20:23 -0500

    added stress to angle calculator

--- python/main.py
+++ python/main.py
@@ -2,15 +2,15 @@
 # ^  <-
 # |   |+M
 # ___>X
-#ASCII Axis :)
+#ASCII Axis 😂
 
 #angle defined from 0,0 in degrees going counter clockwise to the x axsis
 
 
 import math
+import operator
 
-
-
+#2D profile of part as defined by functions
 
 
 def stress(force, area):
@@ -25,5 +25,12 @@ def shearStress(force, angle, area):
     nForce = float(math.sin(math.radians(angle)))*float(force)
     return(stress(nForce,area))
 
+shears = {}
+normal = {}
 
-# for i in range (0,90):
+for i in range (0,90):
+    shears[i]=shearStress(10,i,5)
+    normal[i]=normalStress(10,i,5)
+
+print(shears)
+print(normal)
